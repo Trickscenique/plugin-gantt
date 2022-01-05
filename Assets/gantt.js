dff7df888a46db764715a8a18b5610cab9a503da
Nassim Ourami <27701750+Trickscenique@users.noreply.github.com>
2022-01-05 12:14:40 +0100

    FIx : padding

--- Assets/gantt.js
+++ Assets/gantt.js
@@ -11,7 +11,7 @@ const GanttUtils = {
 			tasks[i].end = end;
 			tasks[i].name = tasks[i].title;
 			tasks[i].progress.replace('%', '');
-			tasks[i].custom_class = 'task-board.color-' + tasks[i].color.name.toLowerCase();
+			tasks[i].custom_class = 'color-' + tasks[i].color.name.toLowerCase();
 		}
 
 		return tasks;
@@ -52,14 +52,12 @@ KB.on('dom.ready', function () {
 		let config = container.dataset;
 
 		let chart = new Gantt('#gantt-chart', GanttUtils.formatTasks(config.records), {
-			header_height: 50,
 			column_width: 30,
 			step: 24,
 			view_modes: ['Quarter Day', 'Half Day', 'Day', 'Week', 'Month'],
 			bar_height: 20,
 			bar_corner_radius: 3,
 			arrow_curve: 5,
-			padding: 18,
 			view_mode: 'Day',
 			date_format: 'YYYY-MM-DD',
 			custom_popup_html: null,
