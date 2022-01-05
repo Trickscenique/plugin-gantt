650582fefbcb40125b9d3fc4bee9a57300c269d8
Nassim Ourami <27701750+Trickscenique@users.noreply.github.com>
2022-01-05 12:00:09 +0100

    Fix: css filename

--- Plugin.php
+++ Plugin.php
@@ -26,7 +26,7 @@ class Plugin extends Base
 //        $this->hook->on('template:layout:js', array('template' => 'plugins/Gantt/Assets/chart.js'));
 
         $this->hook->on('template:layout:js', array('template' => 'plugins/Gantt/Assets/frappe-gantt.min.js'));
-        $this->hook->on('template:layout:css', array('template' => 'plugins/Gantt/Assets/frappe-gantt.gantt.css'));
+        $this->hook->on('template:layout:css', array('template' => 'plugins/Gantt/Assets/frappe-gantt.css'));
 
         $this->hook->on('template:layout:js', array('template' => 'plugins/Gantt/Assets/gantt.js'));
         $this->hook->on('template:layout:css', array('template' => 'plugins/Gantt/Assets/gantt.css'));
