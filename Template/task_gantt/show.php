cf56f20a63f7716116678c784168277c9c4263ff
Nassim Ourami <27701750+Trickscenique@users.noreply.github.com>
2022-01-18 11:37:27 +0100

    qsd

--- Template/task_gantt/show.php
+++ Template/task_gantt/show.php
@@ -42,8 +42,12 @@
             }
             ?>
         <div id="dropdown-task-id-<?= $idattribute ?>" style="display: none;">
-
-
+            <?php if ($elements[0] === "task"): ?>
+                  <?= $this->render('task/dropdown', array('task' => $task, 'redirect' => 'board')) ?>
+            <?php endif ?>
+            <?php if ($elements[0] === "subtask"): ?>
+                <?= $this->render('subtask/menu', array('task' => $task['task'] ?? [], 'subtask' => $task)) ?>
+            <?php endif ?>
         </div>
         <?php endforeach ?>
         <svg
