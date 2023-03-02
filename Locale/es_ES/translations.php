8cd864717bab389337b6eb4e1eed88e306cbc5b3
Bazza <ernesto_bazzano@yahoo.com.ar>
2023-03-02 17:54:16 -0300

    Translate in spanish

--- Locale/es_ES/translations.php
+++ Locale/es_ES/translations.php
@@ -1,18 +1,19 @@
 <?php
 
 return array(
-    // 'Sort by position' => '',
-    // 'Sort by date' => '',
-    // 'Add task' => '',
-    // 'There is no start date or due date for this task.' => '',
-    // 'Moving or resizing a task will change the start and due date of the task.' => '',
-    // 'There is no task in your project.' => '',
-    // 'Gantt chart' => '',
-    // 'Gantt chart for all projects' => '',
-    // 'Gantt chart for this project' => '',
-    // 'Project board' => '',
-    // 'There is no start date or end date for this project.' => '',
-    // 'Projects Gantt chart' => '',
-    // 'Switch to the Gantt chart view' => '',
+    'Sort by position' => 'Ordenar por posición',
+    'Sort by date' => 'Ordenar por fecha',
+    'Add task' => 'Agregar tarea',
+    'There is no start date or due date for this task.' => 'No hay fecha de inicio ni fecha de vencimiento para esta tarea.',
+    'Moving or resizing a task will change the start and due date of the task.' => 'Movilizar o redimensionar una tarea cambiará el inicio y la fecha de inicio de la tarea.',
+    'There is no task in your project.' => 'No hay tarea en su proyecto.',
+    'Gantt chart' => 'Gráfico de Gantt',
+    'Gantt chart for all projects' => 'Gráfico Gantt para todos los proyectos',
+    'Gantt chart for this project' => 'Gráfico Gantt para todos los proyectos',
+    'Project board' => 'Tablero del proyecto',
+    'There is no start date or end date for this project.' => 'No hay fecha de inicio ni fecha de finalización para este proyecto.',
+    'Projects Gantt chart' => 'Proyectos Gráfico',
+    'Switch to the Gantt chart view' => 'Cambiar a la vista del gráfico Gantt',
+
 );
 
